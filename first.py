22909255543b982c7355198505ad32379fcf982a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-10 21:00:22 +0330

    20dey

--- first.py
+++ first.py
@@ -1,3 +1,63 @@
-print("hello every body")
-print('this is our first program in python')
-print('welcome to python class')
+#print("hello every body")
+#print('this is our first program in python')
+#print('welcome to python class')
+
+# انواع داده در پایتون
+# عددی یا number
+# int صحیح
+# اعشاری float
+
+# x = 1
+
+# print('x type is:', type(x))
+
+# y = 1.5
+
+# print('y type is:', type(y))
+
+# نوع داده رشتهای یا متنی یا str
+
+# name = "behrad"
+# print(type(name))
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6]) -> error
+
+# exercise1:
+'''
+string = "abcedfghij"
+رشته بالا رو کاراکتر به کاراکتر نمایش بده 
+با دستور پرینت
+
+
+'''
+
+# exercise2:
+'''
+numbers = '1234567'
+
+هر یک از اعداد رشته بالا را جداگانه در یک خط نمایش بده
+
+
+'''
+
+# قانون نامگذاری متغیرها
+# اسم متغیرها می تواند ترکیبی از حروف، اهداد و _ باشد
+# x , x1 , name1 , name_1, _name
+# اسم متغیر با عدد نمی تواند شروع شود.
+# کاراکترهای خاص هم نمی توان گذاشت
+# @# -
+########################
+description = "hello every body, here is out python class.\nwe are going to learn every thing about python"
+print(description)
+description = """hello every body, here is out python class.
+we are going to learn every thing about python"""
+print(description)
+description = '''hello every body, here is out python class.
+we are going to learn every thing about python'''
+print(description)
