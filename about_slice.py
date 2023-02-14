6f3d81fb654579623ba1400e681d13aab5fd81f2
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-14 20:59:32 +0330

    sadeghi

---
+++ about_slice.py
@@ -0,0 +1,36 @@
+# characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(characters[0:3])
+# print(characters[3:6])
+
+# ['e', 'f', 'g']
+# ['c', 'd', 'e']
+# ['f', 'g']
+# ['b', 'c']
+
+# print(characters[:2]+characters[5:])
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print("odd numbers are: ")
+# print(numbers[::2])
+
+# print("even numbers are:")
+
+# numbers = []
+# ex1 : سه عدد را از ورودی بگیر و در لیست ذخیره کن
+# مجموع اعداد را نمایش بده
+# print("sum of numbers is:")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers.append(10)
+
+del numbers[0]
+numbers.remove(4)
+print(numbers)
+
+# ex 2:
+
+# سه نام از ورودی در یافت کن و در یک لیست قرار بده
+# دومین نام را از داخل لیست حذف کن
+# لیست را پرینت کن.
