916200f6ebfb1863a740621cad31fc10298c7c2b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-21 21:00:44 +0330

    sadeghi

---
+++ review1.py
@@ -0,0 +1,85 @@
+# + -  *  /
+
+# x = 3
+
+# print(x/2)
+# print(x//2)
+
+# print(2**3)
+
+# print(x % 2)
+# print(4 % 2) # ÷    ×
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# n = numbers.pop()
+# print(n)
+# print(numbers)
+# n = numbers.pop(3)
+# print(n)
+# print(numbers)
+
+
+# print('''Hello every body.
+# Welcome to our python class.
+# ''')
+# print('Hello every body.\nWelcome to our python class.')
+# print('Hello every body.\tWelcome to our python class.')
+# print('Hello every body.\rWelcome to our python class.\rBlalalalalallla')
+
+# score = 0
+# score = score + 1
+# print(score)
+
+
+# score += 1
+# print(score)
+# score += 2
+# print(score)
+
+
+# score = score - 1
+# print("new score:", score)
+
+# score -= 1
+# print("last score:", score)
+
+
+# python = 100
+# scratch = 90
+# appinventor = 80
+
+# ave = (python + scratch + appinventor) / 3
+
+# print("average is :", ave)
+
+
+# print(1+2*2)
+# print((1+2)*2)
+
+# print(2*2/2)
+# print(2+2-2)
+
+
+a = 4
+b = 5
+if a < b:
+    print("a is lower than b")
+    print()
+else:
+    print("b is lower than a")
+age = 12
+if age >= 18:
+    print("you are adult!")
+# elif age >= 12 and age < 18:
+elif 12 <= age < 18:
+    print("you are junior!")
+else:
+    print("you are kid!")
+age = 12
+if age == 13:
+    print("you are teenager!")
+if age != 13:
+    print("ok")
+age = 13
+if age == 13 or age == 14 or age == 15:
+    print("ok ....")
