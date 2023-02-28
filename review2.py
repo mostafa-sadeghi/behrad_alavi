f259d1ddc7128c0c03f961769750a1c9d030ba69
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-28 21:00:13 +0330

    sadeghi

---
+++ review2.py
@@ -0,0 +1,42 @@
+# print('hello')
+# print(1+2*3)
+# print((1+2)*3)
+
+# name = input('enter your name: ')
+# score1 = float(input('enter score number 1: '))
+# score2 = float(input('enter score number 2: '))
+# score3 = float(input('enter score number 3: '))
+# average = (score1 + score2 + score3) / 3
+# print(name, "your average is:", average)
+# print(f"{name} your average is: {average:.1f}")
+
+
+# print("hi.\nwelcome to python class.")
+# print("""hi.
+# welcome to python class.""")
+# print('''hi.
+# welcome to python class.''')
+
+# print("id\tname\tscore")
+# print("12\tali\t20")
+
+# print('hi.\rwelcome to our python class.\rwelcome to our pygame.')
+
+score = 0
+print("my score is:", score)
+
+score = score + 1
+print("my score is:", score)
+
+score += 1
+print("my score is:", score)
+
+
+score = score - 1
+print("my score is:", score)
+score -= 1
+print("my score is:", score)
+
+
+# برنامه ای بنویسید که نام، نام خانوادگی و سن یک فرد را از ورودی بگیرد و کنار هم پرینت کند
+# ali rezaei's age is : 30
