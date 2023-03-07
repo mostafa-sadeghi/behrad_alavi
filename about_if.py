c69546a399499521731286c7e3944e73fb0f9aca
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-07 20:45:13 +0330

    sadeghi

---
+++ about_if.py
@@ -0,0 +1,50 @@
+# a = 4
+# b = 5
+
+# if a > b :
+#     print('a is greater than b')
+# else:
+#     print('a is lower than b')
+
+
+# a = 4
+# b = 5
+
+# if a > b:
+#     print('a is greater than b')
+# elif a == b:
+#     print('a is equal to b')
+# else:
+#     print('a is lower than b')
+
+
+# a = 5
+# b = 4
+
+# if a >= b:
+#     print("a is greater than or equal to b")
+
+# if a > b or a == b:
+#     print("a is greater than or equal to b")
+# score = 22
+# if score > 20 and score < 40:
+#     print("go to level 2")
+
+# if 22 < score < 40:
+#     print("go to level 2")
+'''
+# یک برنامه بنویسید که سن داوطلب را دریافت نمایدو
+اگر سن او کمتر از هشت سال بود
+kid
+
+اگر سن بین هشت تا 12 بود
+junior
+
+اگر سن بین 12 تا 18 بود
+teenager
+
+بالاتر از 18
+adult
+
+
+'''
---
+++ review3.py
@@ -0,0 +1,157 @@
+# print(3/2)
+# print(3//2)
+
+
+# print(4 % 2)
+# print(5 % 2)
+
+# average = (90 + 86 + 71 + 100 + 98) / 5
+# print(average)
+
+# x = int(input('enter a number: '))
+# y = int(input('enter a number: '))
+
+# res = x / y
+# print(res)
+
+
+'''
+a = 1
+b = 3
+print(a + b)
+-----------------------------
+a. A+B
+b. a+b
+c. "a+b"
+d. 4
+e. 1+3
+f. Nothing, the code is invalid.
+'''
+
+
+'''
+a = 1
+b = 3
+print("a+b")
+
+
+a. A+B
+b. a+b
+c. "a+b"
+d. The values of a and b added together.
+e. 10
+f. Nothing, the code is invalid.
+
+'''
+
+
+# print("The answer to 10+10 is," 10+10)
+
+# a. The answer to 10+10 is 10+10
+# b. The answer to 10+10 is 20
+# c. The answer to 10+10 is10+10
+# d. The answer to 10+10 is20
+# e. The answer to 10+10 is,10+10
+# f. The answer to 10+10 is,20
+# g. Nothing, the code is invalid.
+
+
+'''
+print("Hello")
+
+a. Hello
+b. "Hello"
+c. Nothing, there is an error in the code.
+d. Nothing, the code is commented out.
+
+
+'''
+
+
+'''
+x = 10
+print("x")
+
+
+a. "x"
+b. x
+c. 10
+d. Nothing
+
+
+
+'''
+
+
+'''
+
+a. 10
+b. 11
+c. "x"
+d. x
+e. Nothing
+
+
+
+'''
+
+
+'''
+
+x = 10 + 6 / 2
+x = x + 1
+print(x)
+
+
+a. 8
+b. 9
+c. 13
+d. 14
+e. Nothing
+
+
+'''
+
+
+'''
+
+x = input("Enter a value:")
+print(x / 2)
+
+
+a. 10
+b. 0
+c. The value the user entered.
+d. The value the user entered divided by two.
+e. An error because the value was not converted to a number.
+
+'''
+
+
+'''
+
+print("Have a "great" day!")
+
+
+a. "Have a "great" day!"
+b. Have a "great" day!
+c. Have a great day!
+d. Nothing, the double quotes make for a syntax error.
+
+'''
+
+print("Save in c:\\new folder")
+'''
+
+print("Save in c:\new folder")
+
+a. Save in c:\new folder
+
+b. Save in c:
+ew folder
+
+c. Save in c:ew folder
+d. Nothing, the escape code makes for a syntax error
+
+
+'''
