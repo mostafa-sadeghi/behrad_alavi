c63764bdff913bcbdcfc82acb75f0df1d0c219a7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-11 20:59:48 +0330

    sadeghi

---
+++ s15.py
@@ -0,0 +1,142 @@
+# a = 4
+# b = 5
+
+# if a < b:
+#     print("a is less than b.")
+
+# if a > b:
+#     print("a is greater than b.")
+
+
+# a = 4
+# b = 4
+
+# if a <= b:
+#     print("a is less than b.")
+
+# if a >= b:
+#     print("a is greater than b.")
+
+
+# a = 4
+# b = 5
+
+# if a == b:
+#     print("1")
+
+# if a != b:
+#     print("2")
+
+
+# a = 1
+
+# if a == 1:
+#     print("1")
+#     print("2")
+
+
+# a = 2
+# b = 3
+# c = 1
+
+# if a < b and a < c:
+#     print("1")
+
+# if a < b or a < c:
+#     print("2")
+
+
+# a = True
+# b = False
+
+# if a or b:
+#     print("1")
+
+# if a and b :
+#     print("2")
+
+# print(not a)
+# print(not b)
+
+# name = input('enter a name:> ').lower()
+# if name == 'behrad' or name == 'amirhossein':
+#     print(f'{name}, welcome to python class')
+# else:
+#     print("you can not come in.")
+
+# تمرین اول
+'''
+برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید و بزرگترین را نمایش دهد
+
+'''
+
+# تمرین دوم
+'''
+برنامه ای بنویسید که نام یک دانش آموز را از ورودی دریافت نماید و
+نیز نمره سه درس اورا بگیرد و معدل او را محاسبه نماید
+اگر معدل بزرگتر از 19 بود
+A را پرینت نماید
+
+اگر معدل بزرگتر از 18 بود
+B را پرینت نماید
+
+اگر معدل بزرگتر از 17 بود
+C 
+را پرینت نماید
+'''
+
+# number = int(input('enter a number:> '))
+
+# if number == 11 or number == 13:
+#     print("number one")
+
+# elif number == 12 or number == 14:
+#     print("number two")
+# else:
+#     print("number three")
+
+# تمرین 3
+'''
+برای مدرسه خود برنامه ای بنویسید که
+نام و معدل دانش آموز را به عنولن ووردی بگیرد 
+و در صورتی که معدل او از 
+18 
+بیشتر بود
+به او پیام زیر را نمایش دهد
+Ali, you can register in this course
+در غیر اینصورت پیام زیر را نمایش دهد
+Ali you can't register in this course because your average is 17
+you'r average mus be greater than 18
+'''
+
+# تمرین 4
+'''
+برنامه ای بنویسید که پنج عدد از ورودی دریافت نماید 
+جمع اعدا زوج وارد شده 
+و نیز جمع اعداد فرد وارد شده را به صورت جداگانه محاسبه و نمایش دهد
+
+عدد زوج عددی است که باقی مانده تقسیم آن بر دو مساوی صفر باشد
+4 % 2 
+عدد فرد عددی است که باقیمانده تقسیم آن بر دو مساوی صفر نباشد
+5 % 2
+
+'''
+
+# برنامه شماره 5
+'''
+برنامه ای بنویسید که دو عدد از ورودی دریافت نماید
+و همینطور یک عملگر را از ورودی دریافت نماید
+منظور از عملگر
++-*/
+
+اگر کاربر 
++ 
+را وارد کرد
+مجموع دو عدد را محاسبه و نمایش دهد
+
+اگر 
+- را وارد کرد
+تفریق را نمایش
+و ....
+
+'''
