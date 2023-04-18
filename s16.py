dc1ce0f2552ff42bfaf4c1e8d7b5b7d66e4cb7af
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-18 20:58:57 +0330

    sadeghi

---
+++ s16.py
@@ -0,0 +1,59 @@
+for i in range(5):
+    print("I want to learn pygame")
+print("outside")
+
+for i in range(5):
+    print(i)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(2,11,2):
+    print(i, end=' ')
+# exercise 1: اعداد فرد بین دویست تا دویست و بیست را پرینت نمائید
+
+
+for i in range(10,0,-1):
+    print(i, end=' ')
+
+for s in 'behrad':
+    print(s, end=' ')
+print()
+for number in [1,2,3,6,9,0,-1]:
+    print(number, end=' ')
+for name in ['amir', 'behrad','tina']:
+    print(name, end=' ')
+
+# exercise 2 :  برنامه ای بنویسید که نام شخصی را از ورودی دریافت نماید و تمامی کاراکترهای ان را با فاصله در یک خط نمایش دهد
+# ex3 : برنامه ای بنویسید که با کمک حلقه فور پنج عدد از ورودی دریاف نماید و اعدا را درون لیستی اضافه کند
+# در نهایت مجموع اعداد لیست را محاسبه و نمایش دهید.
+# numbers = []
+# for 
+# input
+# append
+# sum of numbers
+
+
+# for i in range(3):
+#     print("a")
+# for j in range(3):
+#     print("b")
+
+
+for i in range(3):
+    print("a")
+    for j in range(3):
+        print("b")
+
+
+# ex4 : 
+'''
+with turtle and for loop
+draw these polygons
+مثلث
+مربع
+پنجضلعی
+ششضلعی
+
+
+'''
