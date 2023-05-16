33d588fa8383d875bfd761177ea96a4cadc358d7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-16 20:45:20 +0330

    sadeghi

--- s18.py
+++ s18.py
@@ -1,16 +1,16 @@
-numbers = []
-for i in range(4):
-    new_number = int(input('enter a number: '))
-    numbers.append(new_number)
-print()
-x = int(input('enter a number: '))
-# print(numbers.count(x))
-n = 0
-for c in numbers:
-    if c == x:
-        n += 1
+# numbers = []
+# for i in range(4):
+#     new_number = int(input('enter a number: '))
+#     numbers.append(new_number)
+# print()
+# x = int(input('enter a number: '))
+# # print(numbers.count(x))
+# n = 0
+# for c in numbers:
+#     if c == x:
+#         n += 1
 
-print(n)
+# print(n)
 
 # ex1  :  
 '''
@@ -21,5 +21,64 @@ print(n)
 
 '''
 
-numbers = '123456789'
+# numbers = '123456789'
 # for    جمع   
+
+# print(int(numbers[0])+ int(numbers[1])+ int(numbers[2]))
+# total = 0
+# for n in numbers:
+#     total += int(n)
+
+# print(total)
+
+# total = 0
+
+# for n in numbers:
+#     if int(n) % 2 == 0:
+#         total += int(n)
+
+# print(total)
+
+# favorite_sports = {
+#     'behrad':'football',
+#     'amirhossein':'football'
+# }
+
+# print(favorite_sports['behrad'])
+# print(favorite_sports['amirhossein'])
+
+# favorite_sports = {}
+# for i in range(2):
+#     name = input('enter your name: ')
+#     sport = input('enter your favorite sport: ')
+#     favorite_sports[name] = sport
+
+# print(favorite_sports)
+
+# favorite_sports = {
+#     input('enter a name: '): input('enter a sport'),
+#     input('enter a name: '): input('enter a sport'),
+# }
+# print(favorite_sports)
+
+
+# import turtle
+
+# s = turtle.Screen()
+# pen = turtle.Pen()
+# pen.speed("slowest")
+# pen.color('black')
+# COLORS = ["red","green","blue","orange"]
+# for n in range(4):
+#     pen.fillcolor(COLORS[n])
+#     pen.begin_fill()
+#     for i in range(4):
+#         pen.forward(100)
+#         pen.left(90)
+#     pen.end_fill()
+#     pen.right(90)
+
+
+
+# turtle.done()
+
